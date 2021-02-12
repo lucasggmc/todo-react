256c674caae910261a358ec79127b1cd0670a56c
lucasggmc <lucasggmc@gmail.com>
2021-02-12 18:00:50 -0300

    add styled components and css ajusts

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { General } from './general';
 
 const Navbar = () => {
-    return(
-        <div className="container-fluid">
+    return(        
+        // <>
+        //     <General>teste</General>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">My Todos</a>
+                <img src={"./home-icon.png"} width="40" height="40" style={{margin: 10}}></img>
+                <a className="navbar-brand" href="#">Todo App</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <Link to="/create" className="nav-link">Nova Tarefa</Link>                            
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Pricing</a>
@@ -24,8 +28,8 @@ const Navbar = () => {
                     Navbar text with an inline element
                     </span>
                 </div>
-            </nav>
-        </div>
+            </nav>        
+        // </>
     );
 }
 
---
+++ src/components/general.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const General = styled.div`
+    background-color: red
+`;
+
+
+
